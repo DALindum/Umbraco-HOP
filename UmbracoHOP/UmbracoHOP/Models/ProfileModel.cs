38c7a02164fb8f59a06ec6a41da261bed918f7c1
DALindum <99171380+DALindum@users.noreply.github.com>
2022-11-22 10:58:00 +0100

    Update UmbracoHOP/UmbracoHOP/Models/ProfileModel.cs
    Co-authored-by: Andreas Zerbst <73799582+andr317c@users.noreply.github.com>

--- UmbracoHOP/UmbracoHOP/Models/ProfileModel.cs
+++ UmbracoHOP/UmbracoHOP/Models/ProfileModel.cs
@@ -3,6 +3,7 @@ namespace UmbracoHOP.Models;
 public class ProfileModel
 {
     public int ID { get; set; }
+    
     public string Email { get; set; }
     public string Password { get; set; }
     public string NewPassword { get; set; }
