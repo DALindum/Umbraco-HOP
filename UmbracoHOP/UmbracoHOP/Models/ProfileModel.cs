1843d10ff386bb1a88f23e3520d838a485afc7fe
DALindum <99171380+DALindum@users.noreply.github.com>
2022-11-22 10:59:39 +0100

    Umbraco models (#1)
    * Updated Views
    - _Layout.cshtml
    - Profile.cshtml
    
    * Added Models
    - ProfileModel.cs
    - SearchModel.cs
    
    * Update UmbracoHOP/UmbracoHOP/Models/ProfileModel.cs
    
    Co-authored-by: Andreas Zerbst <73799582+andr317c@users.noreply.github.com>
    
    * Update UmbracoHOP/UmbracoHOP/Models/ProfileModel.cs
    
    Co-authored-by: Andreas Zerbst <73799582+andr317c@users.noreply.github.com>
    
    * Update UmbracoHOP/UmbracoHOP/Models/ProfileModel.cs
    
    Co-authored-by: Andreas Zerbst <73799582+andr317c@users.noreply.github.com>
    
    * Update UmbracoHOP/UmbracoHOP/Models/ProfileModel.cs
    
    Co-authored-by: Andreas Zerbst <73799582+andr317c@users.noreply.github.com>
    
    Co-authored-by: Andreas Zerbst <73799582+andr317c@users.noreply.github.com>

---
+++ UmbracoHOP/UmbracoHOP/Models/ProfileModel.cs
@@ -0,0 +1,13 @@
+namespace UmbracoHOP.Models;
+
+public class ProfileModel
+{
+    public int ID { get; set; }
+    
+    public string Email { get; set; }
+    
+    public string Password { get; set; }
+    
+    public string NewPassword { get; set; }
+    
+}
