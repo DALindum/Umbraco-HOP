fa61ce495842084f151675d8f099bb68c5d4ac08
DALindum <thelindum@gmail.com>
2022-11-22 10:49:17 +0100

    Added Models - ProfileModel.cs - SearchModel.cs

---
+++ UmbracoHOP/UmbracoHOP/Models/ProfileModel.cs
@@ -0,0 +1,9 @@
+namespace UmbracoHOP.Models;
+
+public class ProfileModel
+{
+    public int ID { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string NewPassword { get; set; }
+}
