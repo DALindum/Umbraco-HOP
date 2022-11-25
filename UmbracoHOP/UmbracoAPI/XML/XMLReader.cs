7c356d9331d31e6fb842b21eda4cadfda296c606
DALindum <99171380+DALindum@users.noreply.github.com>
2022-11-25 10:12:15 +0100

    Made some changes in the XMLReader.cs (#5)
    - Can now read from the XML file to get the connectionString
    
    Changes made in the SearchController.cs
    - Shows the ConnectionString

--- UmbracoHOP/UmbracoAPI/XML/XMLReader.cs
+++ UmbracoHOP/UmbracoAPI/XML/XMLReader.cs
@@ -1,6 +1,24 @@
+using System.Xml;
+using System.Linq;
+using System.Xml.Linq;
+
 namespace UmbracoAPI.XML;
 
 public class XMLReader
 {
+    private string xmlPath = @"..\UmbracoAPI\XML\XMLConnection.xml";
     
+    public string GetConnectionString()
+    {
+        XDocument doc = XDocument.Load(xmlPath);
+
+        var elements = doc.Descendants("Config").Elements("connectionString");
+
+        foreach (XElement element in elements)
+        {
+            return element.Attribute("value").Value;
+        }
+
+        return "Failed";
+    }
 }
