81fd9337f4a689d55f02f8fa0847406cca63631b
Andreas Zerbst <andr317c@live.dk>
2022-11-28 09:39:12 +0100

    Added a working api for getting the active install for a day, the value of the day can be changed so it searches for other days

--- UmbracoHOP/UmbracoHOP/Models/LoginModel.cs
+++ UmbracoHOP/UmbracoHOP/Models/LoginModel.cs
@@ -4,12 +4,13 @@ namespace UmbracoHOP.Models;
 
 public class LoginModel
 {
-    public int ID { get; set; }
+    public int Id { get; set; }
     
     [Required(ErrorMessage = "The username field cannot be empty")]
     [StringLength(30, MinimumLength = 3, ErrorMessage = "Username must be between 3 and 30 characters long")]
     public string Username { get; set; }
     
     [Required(ErrorMessage = "The password field cannot be empty")]
+    [StringLength(30, MinimumLength = 6, ErrorMessage = "Password must be between 6 and 30 characters long")]
     public string Password { get; set; }
 }
