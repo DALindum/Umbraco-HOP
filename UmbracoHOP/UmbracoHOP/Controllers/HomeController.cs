8191289fb2d55f2322fdeeae033332d93f9940de
DALindum <thelindum@gmail.com>
2022-12-09 08:30:35 +0100

    Added the API calls for the searching in the HomeController.cs with the overloads as well.
    Added the searching for:
    - Getting PackageName
    - Getting PackageName & PackageVersion
    - Getting the Umbraco Version
    - Getting a specific Country
    - Getting a specific City
    - Getting a Continent

--- UmbracoHOP/UmbracoHOP/Controllers/HomeController.cs
+++ UmbracoHOP/UmbracoHOP/Controllers/HomeController.cs
@@ -22,29 +22,186 @@ public class HomeController : Controller
     public async Task<ActionResult> Homepage()
     {
         string apiUrl = "https://localhost:7209/api/UmbracoInstall/GetAllUmbracoInstalls";
-
+    
         using (HttpClient client = new HttpClient())
         {
             client.BaseAddress = new Uri(apiUrl);
             client.DefaultRequestHeaders.Accept.Clear();
             client.DefaultRequestHeaders.Accept.Add(
                 new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
-
+    
             HttpResponseMessage response = await client.GetAsync(apiUrl);
             if (response.IsSuccessStatusCode)
             {
                 var data = await response.Content.ReadAsStringAsync();
                 var table = Newtonsoft.Json.JsonConvert.DeserializeObject<System.Data.DataTable>(data);
-
-
+    
+    
                 Debug.WriteLine(data);
                 Debug.WriteLine(table);
             }
         }
-
+    
         return View();
     }
 
+    // public async Task<ActionResult> Homepage()
+    // {
+    //     // string apiURL = $"https://localhost:7209/api/UmbracoInstall/GetPackageName?packageName={INPUT}";
+    //     
+    //     using (HttpClient client = new HttpClient())
+    //     {
+    //         client.BaseAddress = new Uri(apiUrl);
+    //         client.DefaultRequestHeaders.Accept.Clear();
+    //         client.DefaultRequestHeaders.Accept.Add(
+    //             new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+    //
+    //         HttpResponseMessage response = await client.GetAsync(apiUrl);
+    //         if (response.IsSuccessStatusCode)
+    //         {
+    //             var data = await response.Content.ReadAsStringAsync();
+    //             var table = Newtonsoft.Json.JsonConvert.DeserializeObject<System.Data.DataTable>(data);
+    //
+    //
+    //             Debug.WriteLine(data);
+    //             Debug.WriteLine(table);
+    //         }
+    //     }
+    //     
+    //     return View();
+    // }
+
+    // public async Task<ActionResult> Homepage()
+    // {
+    //     string apiUrl =
+    //         $"https://localhost:44327/api/UmbracoInstall/GetPackageVersion?packageName={INPUT}&packageVersion={INPUT}";
+    //
+    //     using (HttpClient client = new HttpClient())
+    //     {
+    //         client.BaseAddress = new Uri(apiUrl);
+    //         client.DefaultRequestHeaders.Accept.Clear();
+    //         client.DefaultRequestHeaders.Accept.Add(
+    //             new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+    //
+    //         HttpResponseMessage response = await client.GetAsync(apiUrl);
+    //         if (response.IsSuccessStatusCode)
+    //         {
+    //             var data = await response.Content.ReadAsStringAsync();
+    //             var table = Newtonsoft.Json.JsonConvert.DeserializeObject<System.Data.DataTable>(data);
+    //
+    //
+    //             Debug.WriteLine(data);
+    //             Debug.WriteLine(table);
+    //         }
+    //     }
+    //
+    //     return View();
+    // }
+    //
+    // public async Task<ActionResult> Homepage()
+    // {
+    //     string apiUrl = $"https://localhost:44327/api/UmbracoInstall/GetUmbracoVersion?version={INPUT}";
+    //
+    //     using (HttpClient client = new HttpClient())
+    //     {
+    //         client.BaseAddress = new Uri(apiUrl);
+    //         client.DefaultRequestHeaders.Accept.Clear();
+    //         client.DefaultRequestHeaders.Accept.Add(
+    //             new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+    //
+    //         HttpResponseMessage response = await client.GetAsync(apiUrl);
+    //         if (response.IsSuccessStatusCode)
+    //         {
+    //             var data = await response.Content.ReadAsStringAsync();
+    //             var table = Newtonsoft.Json.JsonConvert.DeserializeObject<System.Data.DataTable>(data);
+    //
+    //
+    //             Debug.WriteLine(data);
+    //             Debug.WriteLine(table);
+    //         }
+    //     }
+    //     
+    //     return View();
+    // }
+    //
+    // public async Task<ActionResult> Homepage()
+    // {
+    //     string apiUrl = $"https://localhost:44327/api/UmbracoInstall/GetCountry?country={INPUT}";
+    //
+    //     using (HttpClient client = new HttpClient())
+    //     {
+    //         client.BaseAddress = new Uri(apiUrl);
+    //         client.DefaultRequestHeaders.Accept.Clear();
+    //         client.DefaultRequestHeaders.Accept.Add(
+    //             new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+    //
+    //         HttpResponseMessage response = await client.GetAsync(apiUrl);
+    //         if (response.IsSuccessStatusCode)
+    //         {
+    //             var data = await response.Content.ReadAsStringAsync();
+    //             var table = Newtonsoft.Json.JsonConvert.DeserializeObject<System.Data.DataTable>(data);
+    //
+    //
+    //             Debug.WriteLine(data);
+    //             Debug.WriteLine(table);
+    //         }
+    //     }
+    //     
+    //     return View();
+    // }
+    //
+    // public async Task<ActionResult> Homepage()
+    // {
+    //     string apiUrl = $"https://localhost:44327/api/UmbracoInstall/GetCity?city={INPUT}";
+    //     
+    //     using (HttpClient client = new HttpClient())
+    //     {
+    //         client.BaseAddress = new Uri(apiUrl);
+    //         client.DefaultRequestHeaders.Accept.Clear();
+    //         client.DefaultRequestHeaders.Accept.Add(
+    //             new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+    //
+    //         HttpResponseMessage response = await client.GetAsync(apiUrl);
+    //         if (response.IsSuccessStatusCode)
+    //         {
+    //             var data = await response.Content.ReadAsStringAsync();
+    //             var table = Newtonsoft.Json.JsonConvert.DeserializeObject<System.Data.DataTable>(data);
+    //
+    //
+    //             Debug.WriteLine(data);
+    //             Debug.WriteLine(table);
+    //         }
+    //     }
+    //     
+    //     return View();
+    // }
+    //
+    // public async Task<ActionResult> Homepage()
+    // {
+    //     string apiUrl = $"https://localhost:44327/api/UmbracoInstall/GetContinent?continent={INPUT}";
+    //
+    //     using (HttpClient client = new HttpClient())
+    //     {
+    //         client.BaseAddress = new Uri(apiUrl);
+    //         client.DefaultRequestHeaders.Accept.Clear();
+    //         client.DefaultRequestHeaders.Accept.Add(
+    //             new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+    //
+    //         HttpResponseMessage response = await client.GetAsync(apiUrl);
+    //         if (response.IsSuccessStatusCode)
+    //         {
+    //             var data = await response.Content.ReadAsStringAsync();
+    //             var table = Newtonsoft.Json.JsonConvert.DeserializeObject<System.Data.DataTable>(data);
+    //
+    //
+    //             Debug.WriteLine(data);
+    //             Debug.WriteLine(table);
+    //         }
+    //     }
+    //     
+    //     return View();
+    // }
+    
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
