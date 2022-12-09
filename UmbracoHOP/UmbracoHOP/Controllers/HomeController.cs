b8715b90c39cf8d1c32fa2cf5b9da9ee3312f71b
DALindum <thelindum@gmail.com>
2022-12-09 08:54:18 +0100

    Added another way of calling the API so it adds to the model.

--- UmbracoHOP/UmbracoHOP/Controllers/HomeController.cs
+++ UmbracoHOP/UmbracoHOP/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using System.Diagnostics;
+using System.Net.Http.Headers;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using UmbracoHOP.Models;
 
 namespace UmbracoHOP.Controllers;
@@ -19,31 +21,58 @@ public class HomeController : Controller
     //     return View();
     // }
 
-    public async Task<ActionResult> Homepage()
-    {
-        string apiUrl = "https://localhost:7209/api/UmbracoInstall/GetAllUmbracoInstalls";
-    
-        using (HttpClient client = new HttpClient())
-        {
-            client.BaseAddress = new Uri(apiUrl);
-            client.DefaultRequestHeaders.Accept.Clear();
-            client.DefaultRequestHeaders.Accept.Add(
-                new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
-    
-            HttpResponseMessage response = await client.GetAsync(apiUrl);
-            if (response.IsSuccessStatusCode)
-            {
-                var data = await response.Content.ReadAsStringAsync();
-                var table = Newtonsoft.Json.JsonConvert.DeserializeObject<System.Data.DataTable>(data);
-    
-    
-                Debug.WriteLine(data);
-                Debug.WriteLine(table);
-            }
-        }
-    
-        return View();
-    }
+    private string apiUrl = "https://localhost:7209/";
+
+    // public async Task<ActionResult> Homepage()
+    // {
+    //     List<UmbracoInstallsModel> UmbracoInstallsInfo = new List<UmbracoInstallsModel>();
+    //
+    //     using (var client = new HttpClient())
+    //     {
+    //         client.BaseAddress = new Uri(apiUrl);
+    //         client.DefaultRequestHeaders.Clear();
+    //
+    //         client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+    //
+    //         HttpResponseMessage response = await client.GetAsync("api/UmbracoInstall/GetAllUmbracoInstalls");
+    //
+    //         if (response.IsSuccessStatusCode)
+    //         {
+    //             var UmbracoInstallsResponse = response.Content.ReadAsStringAsync().Result;
+    //
+    //             UmbracoInstallsInfo =
+    //                 JsonConvert.DeserializeObject<List<UmbracoInstallsModel>>(UmbracoInstallsResponse);
+    //         }
+    //
+    //         return View(UmbracoInstallsInfo);
+    //     }
+    // }
+
+    // public async Task<ActionResult> Homepage()
+    // {
+    //     string apiUrl = "https://localhost:7209/api/UmbracoInstall/GetAllUmbracoInstalls";
+    //
+    //     using (HttpClient client = new HttpClient())
+    //     {
+    //         client.BaseAddress = new Uri(apiUrl);
+    //         client.DefaultRequestHeaders.Accept.Clear();
+    //         client.DefaultRequestHeaders.Accept.Add(
+    //             new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+    //
+    //         HttpResponseMessage response = await client.GetAsync(apiUrl);
+    //         if (response.IsSuccessStatusCode)
+    //         {
+    //             var data = await response.Content.ReadAsStringAsync();
+    //             var table = Newtonsoft.Json.JsonConvert.DeserializeObject<System.Data.DataTable>(data);
+    //
+    //
+    //             Debug.WriteLine(data);
+    //             Debug.WriteLine(table);
+    //         }
+    //     }
+    //
+    //     return View();
+    // }
 
     // public async Task<ActionResult> Homepage()
     // {
@@ -201,7 +230,7 @@ public class HomeController : Controller
     //     
     //     return View();
     // }
-    
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
