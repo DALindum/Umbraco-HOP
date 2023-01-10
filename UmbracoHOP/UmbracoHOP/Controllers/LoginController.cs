abd00970bce6b215e64ec017dabaafaa267c7eec
DALindum <thelindum@gmail.com>
2023-01-10 14:15:29 +0100

    Begun to create the login feature
    - LoginController.cs
    
    I sent the database

--- UmbracoHOP/UmbracoHOP/Controllers/LoginController.cs
+++ UmbracoHOP/UmbracoHOP/Controllers/LoginController.cs
@@ -1,3 +1,7 @@
+using System.Data;
+using System.Data.SqlClient;
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using UmbracoHOP.Models;
 
@@ -18,9 +22,35 @@ public class LoginController : Controller
     {
         if (ModelState.IsValid)
         {
+            using (SqlConnection connection = new SqlConnection(@"Server=(localdb)\UmbracoDev"))
+            {
+                string queryLog = "SELECT username, password FROM users WHERE username = @username and password = @password";
+            
+                using (SqlCommand command = new SqlCommand(queryLog, connection))
+                {
+                    command.Parameters.AddWithValue("@username", login.Username);
+                    command.Parameters.AddWithValue("@password", login.Password);
+
+                    connection.Open();
+                    SqlDataAdapter adpt = new SqlDataAdapter(command);
+                    DataSet dts = new DataSet();
+                    adpt.Fill(dts);
+                    connection.Close();
+                }
+            }
+            
             return RedirectToAction("Homepage", "Home");
         }
 
         return View();
+        
+        /*
+        if (ModelState.IsValid)
+        {
+            return RedirectToAction("Homepage", "Home");
+        }
+
+        return View();
+         */
     }
 }
