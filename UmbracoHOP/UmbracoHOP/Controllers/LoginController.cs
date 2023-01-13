cda2c9de555e0ee147c54736f493dca3990520dc
DALindum <thelindum@gmail.com>
2023-01-13 11:05:01 +0100

    Added something  to test

--- UmbracoHOP/UmbracoHOP/Controllers/LoginController.cs
+++ UmbracoHOP/UmbracoHOP/Controllers/LoginController.cs
@@ -22,8 +22,10 @@ public class LoginController : Controller
     {
         if (ModelState.IsValid)
         {
+            /*
             using (SqlConnection connection = new SqlConnection(@"Server=(localdb)\UmbracoDev"))
             {
+                
                 string query = $"SELECT username, password FROM users WHERE username = {login.Username} and password = {login.Password}";
             
                 using (SqlCommand command = new SqlCommand(query, connection))
@@ -38,12 +40,33 @@ public class LoginController : Controller
                     connection.Close();
                 }
             }
-            
-            return RedirectToAction("Homepage", "Home");
-        }
+            */
 
+            SqlConnection connection = new SqlConnection(@"Server=(localdb)\UmbracoDev");
+            SqlCommand command =
+                new SqlCommand(
+                    "SELECT username, password FROM users WHERE username = @username and password = @password");
+
+            command.Parameters.AddWithValue("@username", login.Username);
+            command.Parameters.AddWithValue("@password", login.Password);
+
+            SqlDataAdapter sda = new SqlDataAdapter(command);
+
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+
+            connection.Open();
+            int i = command.ExecuteNonQuery();
+            connection.Close();
+
+            if (dt.Rows.Count > 0)
+            {
+                return RedirectToAction("Homepage", "Home");
+            }
+
+        }
         return View();
-        
+
         /*
         if (ModelState.IsValid)
         {
